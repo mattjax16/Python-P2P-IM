753de2f3807a80c61afb87f9b4e65592b328ba93
Matt (Homelab Proxmox 3060) <mattjax16@gmail.com>
2025-02-02 14:57:02 -0500

    addded shutdown signal handeling to reuse ports

--- unencryptedim.py
+++ unencryptedim.py
@@ -13,16 +13,17 @@ import signal
 
 def get_args():
     parser = argparse.ArgumentParser()
-    group = parser.add_mutually_exclusive_group(required = True)
-    group.add_argument('--s', action="store_true")
-    group.add_argument('--c', metavar='hostname', type=str)
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument("--s", action="store_true")
+    group.add_argument("--c", metavar="hostname", type=str)
     args = parser.parse_args()
     return args
 
 
 def server():
+    signal.signal(signal.SIGINT, shutdown)
     server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_sock.bind(('0.0.0.0',9999))
+    server_sock.bind(("0.0.0.0", 9999))
     server_sock.listen(1)
     while True:
         client_sock, client_addr = server_sock.accept()
@@ -30,7 +31,7 @@ def server():
 
         try:
             while True:
-                msg = client_sock.recv(2048).decode('utf-8')
+                msg = client_sock.recv(2048).decode("utf-8")
                 print(msg)
 
         except KeyboardInterrupt:
@@ -42,13 +43,14 @@ def server():
 
 
 def client(hostname):
+    signal.signal(signal.SIGINT, shutdown)
     client_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client_sock.connect((hostname, 9999))
 
     try:
         while True:
             msg = input()
-            client_sock.sendall(msg.encode('utf-8'))
+            client_sock.sendall(msg.encode("utf-8"))
 
     except KeyboardInterrupt:
         pass
@@ -57,9 +59,10 @@ def client(hostname):
         sys.exit(0)
 
 
-def shutdown():
-
+def shutdown(signum, frame):
+    sys.exit(0)
     return
+
 
 def main():
     args = get_args()
@@ -70,12 +73,6 @@ def main():
         server()
 
     print("done")
-
-
-
-
-
-
 
 
 if __name__ == "__main__":
