95970f652543289c554475424f939cb0e6713520
Matt (Homelab Proxmox 3060) <mattjax16@gmail.com>
2025-02-02 18:10:02 -0500

    working transfers filke verbatim fixxed by not having a \n when printing and not stripping

--- unencryptedim.py
+++ unencryptedim.py
@@ -30,18 +30,16 @@ def p2p_message_handler(client_sock):
                     msg = client_sock.recv(20480).decode("utf-8")
                     if not msg:
                         return
-                    print(msg)
-                except:
+                    print(msg, end="")
+                except BaseException:
                     return
             else:
                 try:
                     msg = stdin.readline()
                     if not msg:
                         return
-
-
                     client_sock.sendall(msg.encode("utf-8"))
-                except:
+                except BaseException:
                     return
 
 
@@ -51,7 +49,6 @@ def server():
     server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_sock.bind(("0.0.0.0", 9999))
     server_sock.listen(1)
-
     client_sock, addr = server_sock.accept()
     p2p_message_handler(client_sock)
 
@@ -61,7 +58,6 @@ def client(hostname):
     signal.signal(signal.SIGTERM, shutdown)
     client_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client_sock.connect((hostname, 9999))
-
     p2p_message_handler(client_sock)
 
 
