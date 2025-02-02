d482a36aa9027d34ad7d5de032258bc7425bf2f7
Matt (Homelab Proxmox 3060) <mattjax16@gmail.com>
2025-02-02 13:57:32 -0500

    added args parsing and tested

--- unencryptedim.py
+++ unencryptedim.py
@@ -1,7 +1,51 @@
-'''
+"""
 unencryptedim.py
 
 Author: Matt Bass
-'''
+"""
+
+import argparse
+import socket
+import sys
+import select
+import signal
 
 
+def get_args():
+    parser = argparse.ArgumentParser()
+    group = parser.add_mutually_exclusive_group(required = True)
+    group.add_argument('--s', action="store_true")
+    group.add_argument('--c', metavar='hostname', type=str)
+    args = parser.parse_args()
+    return args
+
+
+def server():
+    return
+
+
+def client():
+    return
+
+
+def shutdown():
+
+    return
+
+def main():
+    args = get_args()
+
+    if args.c:
+        client()
+    elif args.s:
+        server()
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
