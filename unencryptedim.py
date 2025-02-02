6fa3238a8a94a0ab31806197e5b159eb5a2a32fb
Matt (Homelab Proxmox 3060) <mattjax16@gmail.com>
2025-02-02 15:48:19 -0500

    working on client

--- unencryptedim.py
+++ unencryptedim.py
@@ -9,6 +9,7 @@ import socket
 import sys
 import select
 import signal
+from sys import stdin
 
 
 def get_args():
@@ -20,43 +21,43 @@ def get_args():
     return args
 
 
+
+
 def server():
     signal.signal(signal.SIGINT, shutdown)
     server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_sock.bind(("0.0.0.0", 9999))
     server_sock.listen(1)
+
     while True:
-        client_sock, client_addr = server_sock.accept()
-        print("Connection From", client_addr)
-
-        try:
-            while True:
-                msg = client_sock.recv(2048).decode("utf-8")
-                print(msg)
-
-        except KeyboardInterrupt:
-            pass
-        finally:
-            client_sock.close()
-            server_sock.close()
-            sys.exit(0)
+        inputs, _ , _ = select.select([stdin, server_sock], [], [])
+        for input in inputs:
+            if input == server_sock:
+                try:
+                    msg = server_sock.recv(2048).decode("utf-8")
+                    print(msg)
+                except:
+                    pass
+            else:
+                try:
+                    msg = stdin.readline()
+                    server_sock.sendall(msg.encode("utf-8"))
+                except:
+                    pass
 
 
 def client(hostname):
     signal.signal(signal.SIGINT, shutdown)
     client_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client_sock.connect((hostname, 9999))
-
-    try:
-        while True:
+    while True:
+        try:
             msg = input()
             client_sock.sendall(msg.encode("utf-8"))
 
-    except KeyboardInterrupt:
-        pass
-    finally:
-        client_sock.close()
-        sys.exit(0)
+        except:
+            pass
+
 
 
 def shutdown(signum, frame):
