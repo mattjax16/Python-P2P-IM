398687634caad65cf2bb6842124e50b5d1f7e6fc
Matt (Homelab Proxmox 3060) <mattjax16@gmail.com>
2025-02-03 14:11:07 -0500

    fixxed dumb var mistake

--- unencryptedim.py
+++ unencryptedim.py
@@ -40,7 +40,7 @@ def p2p_message_handler(client_sock):
                         if not msg:
                             return
                         print(msg, end="")
-                    except BaseException:
+                    except:
                         return
                 else:
                     try:
@@ -48,7 +48,7 @@ def p2p_message_handler(client_sock):
                         if not msg:
                             return
                         client_sock.sendall(msg.encode("utf-8"))
-                    except BaseException:
+                    except:
                         return
     finally:
         client_sock.close()
@@ -56,16 +56,16 @@ def p2p_message_handler(client_sock):
 
 def server():
     global server_socket
+    global client_socket
     signal.signal(signal.SIGINT, shutdown)
     signal.signal(signal.SIGTERM, shutdown)
-    server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket = server_sock
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     try:
-        server_sock.bind(("localhost", 9999))
-        server_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        server_sock.listen(1)
-        client_sock, addr = server_sock.accept()
-        p2p_message_handler(client_sock)
+        server_socket.bind(("localhost", 9999))
+        server_socket.listen(1)
+        client_socket, addr = server_socket.accept()
+        p2p_message_handler(client_socket)
     finally:
         pass
         # if client_socket:
@@ -78,11 +78,10 @@ def client(hostname):
     global client_socket
     signal.signal(signal.SIGINT, shutdown)
     signal.signal(signal.SIGTERM, shutdown)
-    client_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
-        client_sock.connect((hostname, 9999))
-        client_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        p2p_message_handler(client_sock)
+        client_socket.connect((hostname, 9999))
+        p2p_message_handler(client_socket)
     finally:
         pass
         # if client_socket:
